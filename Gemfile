fde648877b676fac044f8ff853c3516413a992aa
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-01-18 19:51:25 +0200

    Set up application framework

---
+++ Gemfile
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "~> 3.2"
+
+# ActiveSupport
+gem "activesupport"
+
+# Code loader
+gem "zeitwerk"
+
+group :development do
+  # Debugger
+  gem "debug", require: false
+end
---
+++ lib/birdnest.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "logger"
+
+require "active_support/all"
+require "zeitwerk"
+
+require "debug" if ENV["ENV"] == "development"
+
+module Birdnest
+  class << self
+    attr_reader :loader
+
+    def root
+      @root ||= Pathname.new(File.expand_path(File.join("..", ".."), __FILE__))
+    end
+
+    def logger
+      @logger ||= Logger.new
+    end
+
+    def setup
+      @loader = Zeitwerk::Loader.for_gem
+
+      # Register inflections
+      require root.join("config/inflections.rb")
+
+      # Set up code loader
+      loader.enable_reloading
+      loader.setup
+      loader.eager_load
+    end
+  end
+end
+
+def reload!
+  Birdnest.loader.reload
+end
+
+Birdnest.setup
