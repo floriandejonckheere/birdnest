f55852ee3cba4cc53b374d8d301be45d2e76bf69
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-01-18 21:49:45 +0200

    Render pilot names

--- lib/birdnest/server.rb
+++ lib/birdnest/server.rb
@@ -5,15 +5,15 @@ require "sinatra/base"
 module Birdnest
   class Server < Sinatra::Base
     get "/" do
-      "hello world"
+      erb :index, locals: { pilots: }
     end
 
-    get "/pilots" do
-      content_type :json
+    private
 
+    def pilots
       Birdnest.redis.call("KEYS", "pilot:*").map do |key|
-        Birdnest.redis.call("HGETALL", key)
-      end.to_json
+        Birdnest.redis.call("HGETALL", key).symbolize_keys
+      end
     end
   end
 end
